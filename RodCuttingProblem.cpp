94fb4c81af1e595ab1a1949def1f183a72806d1e
Kartik Mathur <60697152+scaleptica@users.noreply.github.com>
2022-08-22 16:52:11 +0530

    Update RodCuttingProblem.cpp

--- RodCuttingProblem.cpp
+++ RodCuttingProblem.cpp
@@ -1,5 +1,31 @@
 // DP table=>
-
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+        int dp[n+1][n+1];
+        
+        for(int i=0; i<=n; i++){
+            dp[i][0]= 0;
+        }
+        
+        for(int j=0; j<=n; j++){
+            dp[0][j]= 0;
+        }
+        
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i>j){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], price[i-1]+dp[i][j-i]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
 
 
 /*      //memoization=>
